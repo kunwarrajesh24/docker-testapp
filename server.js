4604b6c35dd4f418f65bc28b55d5fbc234ae9c2c
Kunwar Rajesh <kunwarrajesh2407@gmail.com>
2026-02-26 22:57:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,42 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+// 👇 USE IT HERE (Database Configuration)
+const MONGO_URL = "mongodb://admin:qwerty@mongo:27017/testdb?authSource=admin";
+
 const app = express();
-const { MongoClient } = require("mongodb");
 
-const PORT = 5050;
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+// 👇 MongoDB Connection
+mongoose.connect(MONGO_URL)
+  .then(() => {
+    console.log("MongoDB Connected Successfully");
+  })
+  .catch((err) => {
+    console.log("MongoDB Connection Error:", err);
+  });
 
-// Use container name 'mongo' as hostname
-const MONGO_URL = "mongodb://admin:qwerty@mongo:27017/?authSource=admin";
-const client = new MongoClient(MONGO_URL);
+app.get("/", (req, res) => {
+  res.send("Node + MongoDB + Docker is working!");
+});
 
-// Connect once
-async function main() {
-  try {
-    await client.connect();
-    console.log("✔️ Connected to MongoDB");
-
-    const db = client.db("apnacollege-db");
-
-    // GET all users
-    app.get("/getUsers", async (req, res) => {
-      const data = await db.collection("users").find({}).toArray();
-      res.json(data);
-    });
-
-    // POST new user
-    app.post("/addUser", async (req, res) => {
-      const userObj = req.body;
-      const result = await db.collection("users").insertOne(userObj);
-      res.json({ inserted: result.insertedId });
-    });
-
-    app.listen(PORT, () => {
-      console.log(`🚀 Server listening at http://localhost:${PORT}`);
-    });
-  } catch (err) {
-    console.error(err);
-  }
-}
-
-main();
+app.listen(5050, () => {
+  console.log("Server running on port 5050");
+});
